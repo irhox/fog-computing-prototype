56bd99fb5ce958a11324c41fca5d572daaddc0de
Yevhenii Klymenko <klymenko@campus.tu-berlin.de>
2024-07-06 15:44:49 +0200

    feat: create db for sensor data

--- cloud_component/sensor_data.py
+++ cloud_component/sensor_data.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+import sqlite3
 
 
 @dataclass
@@ -8,3 +9,23 @@ class SensorData:
     start_timestamp: str
     end_timestamp: str
     status: str
+
+
+conn = sqlite3.connect('sensordata.db')
+
+cursor = conn.cursor()
+
+create_table_query = """
+CREATE TABLE IF NOT EXISTS SensorData (
+    average_fuel_level REAL,
+    average_power_level REAL,
+    start_timestamp TEXT,
+    end_timestamp TEXT,
+    status TEXT
+);
+"""
+
+cursor.execute(create_table_query)
+
+conn.commit()
+conn.close()
