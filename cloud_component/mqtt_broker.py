aef76a85a0e0d2e6e993bcb352a1cff20a8ef9d9
Yevhenii Klymenko <klymenko@campus.tu-berlin.de>
2024-07-08 15:59:40 +0200

    feat: receive messages from the local node

--- cloud_component/mqtt_broker.py
+++ cloud_component/mqtt_broker.py
@@ -2,6 +2,9 @@ import asyncio
 import websockets
 import json
 
+from cloud_component.sensor_data import SensorData
+from cloud_component.sensor_data_dbmanager import insert_sensor_data
+
 clients = {}
 
 
@@ -15,6 +18,19 @@ async def handle_client(websocket, path):
             data = json.loads(message)
             topic = data['topic']
             payload = data['payload']
+
+            if topic == "power-station/data":
+                aggregated_data_list = json.loads(payload)
+                for aggregated_data in aggregated_data_list:
+                    sensor_data = SensorData(
+                        average_fuel_level=aggregated_data['average_fuel_level'],
+                        average_power_level=aggregated_data['average_power_level'],
+                        start_timestamp=aggregated_data['start_timestamp'],
+                        end_timestamp=aggregated_data['end_timestamp'],
+                        status=aggregated_data['status']
+                    )
+                    insert_sensor_data(sensor_data)
+                    print(f"Inserted sensor data: {sensor_data}")
 
             # Broadcast the message to all clients
             for cid, ws in clients.items():
